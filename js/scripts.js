e4227d4f0f9d28cbbc69e67fbe20040c7a0a9709
Donovan Weber <donovanweber03@gmail.com>
2022-06-01 14:02:46 -0400

    Add buttons for dark and light modes
    Co-authored-by: GarrettHays <GarrettHays@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
   $(".clickable").click(function(){
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
+  });
+
+  $("button#darkBackground").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-mode");
+    });
+
+
+  $("button#lightBackground").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-mode");
   });
 });
